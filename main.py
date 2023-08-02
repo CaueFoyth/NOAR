1286b6fe3d8aca43487571a4511c6d3326dff45b
foythcaue@gmail.com <CaueFoyth>
2023-08-02 11:54:23 -0300

    servidor

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    #Para atualizar automaticamente no localhost coloque debug=True dentro do run
+    app.run(debug=True)
