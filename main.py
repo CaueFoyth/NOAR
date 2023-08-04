164d270db95e944b9d89520662322766439fb0b2
foythcaue@gmail.com <CaueFoyth>
2023-08-04 07:56:33 -0300

    a

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # Instalar flask na maquina virtual
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     #Para atualizar automaticamente no localhost coloque debug=True dentro do run
