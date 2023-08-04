b3d95dd7df7f974ab5ae657973ac6a141d21d5c0
foythcaue@gmail.com <CaueFoyth>
2023-08-04 09:06:34 -0300

    A

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/submit', methods=["POST"])
+def submit():
+    if request.method == "POST":
+        cpf = request.form["cpf"]
+        senha = request.form["senha"]
+        print(cpf,senha)
+        if cpf == '' or senha == '':
+            return render_template('index.html', message="Informe os dados")
+        return "Vai se fude" 
 
 if __name__ == '__main__':
     #Para atualizar automaticamente no localhost coloque debug=True dentro do run
