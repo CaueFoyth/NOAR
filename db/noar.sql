9215a40b3cba865596f0495a0a1aed154e0d7985
foythcaue@gmail.com <CaueFoyth>
2023-08-24 09:57:35 -0300

    db

--- db/noar.sql
+++ db/noar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 22-Ago-2023 às 17:20
+-- Tempo de geração: 24-Ago-2023 às 14:57
 -- Versão do servidor: 10.4.28-MariaDB
 -- versão do PHP: 8.2.4
 
@@ -31,15 +31,40 @@ CREATE TABLE `login` (
   `id_sos` int(11) NOT NULL,
   `cpf_sos` varchar(45) NOT NULL,
   `senha_sos` varchar(45) NOT NULL,
-  `adm` int(1) NOT NULL
+  `adm` int(1) NOT NULL,
+  `nome_sos` varchar(45) NOT NULL,
+  `email_sos` varchar(45) NOT NULL,
+  `logado` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Extraindo dados da tabela `login`
 --
 
-INSERT INTO `login` (`id_sos`, `cpf_sos`, `senha_sos`, `adm`) VALUES
-(1, '125.413.329-12', '123', 0);
+INSERT INTO `login` (`id_sos`, `cpf_sos`, `senha_sos`, `adm`, `nome_sos`, `email_sos`, `logado`) VALUES
+(1, '125.413.329-12', '123', 0, 'Cauê Marchi Foyth', 'foythcaue@gmail.com', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ocorrencias`
+--
+
+CREATE TABLE `ocorrencias` (
+  `id_ocorrencia` int(11) NOT NULL,
+  `fk_sos` int(11) NOT NULL,
+  `data_oco` int(11) NOT NULL,
+  `nome_vit` int(11) NOT NULL,
+  `sexo_vit` varchar(45) NOT NULL,
+  `idade_vit` int(3) NOT NULL,
+  `cpf_vit` varchar(45) NOT NULL,
+  `local_oco` varchar(45) NOT NULL,
+  `acompanhante` int(1) NOT NULL,
+  `nome_acomp` varchar(45) NOT NULL,
+  `idade_acomp` int(3) NOT NULL,
+  `cpf_acomp` varchar(45) NOT NULL,
+  `sexo_acomp` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tabelas despejadas
@@ -52,6 +77,13 @@ ALTER TABLE `login`
   ADD PRIMARY KEY (`id_sos`);
 
 --
+-- Índices para tabela `ocorrencias`
+--
+ALTER TABLE `ocorrencias`
+  ADD PRIMARY KEY (`id_ocorrencia`),
+  ADD KEY `fk_sos` (`fk_sos`);
+
+--
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
@@ -60,6 +92,22 @@ ALTER TABLE `login`
 --
 ALTER TABLE `login`
   MODIFY `id_sos` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de tabela `ocorrencias`
+--
+ALTER TABLE `ocorrencias`
+  MODIFY `id_ocorrencia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `ocorrencias`
+--
+ALTER TABLE `ocorrencias`
+  ADD CONSTRAINT `fk_sos` FOREIGN KEY (`fk_sos`) REFERENCES `login` (`id_sos`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
