6f0d9c417a728bd11b5485f35c357b1841a0fcfb
gabrielmoratelli <gabriel4up@gmail.com>
2023-09-05 11:31:40 -0300

    A

--- admPage.js
+++ admPage.js
@@ -2,11 +2,14 @@
     document.getElementById("open").onclick = function(){
         document.getElementById("float_menu").style.display = "flex";
         document.getElementById("inputs").style.display = "flex";
-        document.getElementById("background_blur").style.height = "100vh";
     }
 
     document.getElementById("close").onclick = function(){
         document.getElementById("float_menu").style.display = "none";
-        document.getElementById("inputs").style.display = "none";
-        document.getElementById("background_blur").style.height = "0vh";
+        document.getElementById("inputs").style.display = "none";   
+    }
+
+    document.getElementById("read").onclick = function(){
+        document.getElementById("read_profile_menu").style.display = "flex";
+        document.getElementById("inputs_read").style.display = "flex";
     }
