b7ee2f5118fee593fa8e139e5db98e115e0c2aae
gabrielmoratelli <gabriel4up@gmail.com>
2023-09-19 08:39:34 -0300

    CRUD Funcional

--- static/admPage.js
+++ static/admPage.js
@@ -9,6 +9,16 @@
         document.getElementById("inputs").style.display = "none";   
     }
 
+    document.getElementById("read").onclick = function(){
+        document.getElementById("read_profile_menu").style.display = "flex";
+        document.getElementById("inputs_read").style.display = "flex";
+    }
+
+    document.getElementById("close-read").onclick = function(){
+        document.getElementById("read_profile_menu").style.display = "none";
+        document.getElementById("inputs_read").style.display = "none";   
+    }
+
     document.getElementById("update").onclick = function(){
         document.getElementById("update_profile_menu").style.display = "flex";
         document.getElementById("inputs_update").style.display = "flex";
